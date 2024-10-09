c1fb64f51e2d99b8b469e52445113ecf52354ca9
sefaca <sefaca24@gmail.com>
2024-10-09 18:39:35 +0200

    Refactorv2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ProductList from './components/ProductList';
+import ProductList from './components/ProductList/ProductList';
 
 function App() {
   return (
